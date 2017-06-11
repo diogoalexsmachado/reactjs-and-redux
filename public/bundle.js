4286353ee3429f06c146f99cdb5351848b1e3a5b
Diogo <diogoalexsmachado@gmail.com>
2017-06-11 23:56:47 +0100

    things

--- public/bundle.js
+++ public/bundle.js
@@ -1,6 +1,21 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
+//actions
+//ADD_DECK
+//SHOW_ADD_DECK
+//HIDE_ADD_DECK
+
+var addDeck = function addDeck(name) {
+  return { type: 'ADD_DECK', data: name };
+};
+var showAddDeck = function showAddDeck() {
+  return { type: 'SHOW_ADD_DECK' };
+};
+var hideAddDeck = function hideAddDeck() {
+  return { type: 'HIDE_ADD_DECK' };
+};
+
 var cards = function cards(state, action) {
   switch (action.type) {
     case 'ADD_CARD':
@@ -13,20 +28,46 @@ var cards = function cards(state, action) {
   }
 };
 
-var decks = function decks(state, action) {
+var deck = function deck(state, action) {
   switch (action.type) {
-    case 'ADD_CARD':
-      state.concat([Object.assign({}, action.data, {
-        id: +new Date()
-      })]);
+    case 'ADD_DECK':
+      var newDeck = { name: action.data, id: +new Date() };
+      return state.concat([newDeck]);
     default:
       return state || [];
   }
 };
 
+var addingDeck = function addingDeck(state, action) {
+  switch (action.type) {
+    case 'SHOW_ADD_DECK':
+      return true;
+    case 'HIDE_ADD_DECK':
+      return false;
+    default:
+      return !!state;
+
+  }
+};
+
+/*
+const decks = (state,action) => {
+  switch(action.type){
+    case 'ADD_DECK':
+        state.concat([
+          Object.assign({}, action.data, {
+            id: +new Date
+          })
+      ]);
+    default:
+      return state || [];
+  }
+};*/
+
 var store = Redux.createStore(Redux.combineReducers({
   cards: cards,
-  decks: decks
+  deck: deck,
+  addingDeck: addingDeck
 }));
 
 //retorna false se não houverem cartas
@@ -71,10 +112,28 @@ var Sidebar = React.createClass({
   }
 });
 
-ReactDOM.render(React.createElement(
-  App,
-  null,
-  React.createElement(Sidebar, { decks: [{ name: 'Deck 1' }], addingDeck: false })
-), document.getElementById('root'));
+function run() {
+  console.log(state);
+  var state = store.getState();
+  ReactDOM.render(React.createElement(
+    App,
+    null,
+    React.createElement(Sidebar, { decks: state.decks, addingDeck: state.addingDeck })
+  ), document.getElementById('root'));
+}
+
+run();
+
+store.subscribe(run);
+
+window.show = function () {
+  return store.dispatch(showAddDeck());
+};
+window.hide = function () {
+  return store.dispatch(hideAddDeck());
+};
+window.add = function () {
+  return store.dispatch(addDeck(new Date().toString()));
+};
 
 },{}]},{},[1]);
--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
+//actions
+//ADD_DECK
+//SHOW_ADD_DECK
+//HIDE_ADD_DECK
+
+const addDeck = name =>({ type: 'ADD_DECK', data: name });
+const showAddDeck = () => ({ type:'SHOW_ADD_DECK' });
+const hideAddDeck = () => ({ type:'HIDE_ADD_DECK' });
+
+
 const cards = (state,action) => {
   switch(action.type){
     case 'ADD_CARD':
@@ -12,9 +22,29 @@ const cards = (state,action) => {
   }
 };
 
+const deck = (state,action) => {
+  switch(action.type){
+    case 'ADD_DECK':
+      let newDeck = { name: action.data, id: +new Date };
+      return state.concat([newDeck]);
+    default:
+      return state || [];
+  }
+};
+
+const addingDeck = (state, action) => {
+  switch (action.type) {
+    case 'SHOW_ADD_DECK': return true;
+    case 'HIDE_ADD_DECK': return false;
+    default: return !!state;
+
+  }
+};
+
+/*
 const decks = (state,action) => {
   switch(action.type){
-    case 'ADD_CARD':
+    case 'ADD_DECK':
         state.concat([
           Object.assign({}, action.data, {
             id: +new Date
@@ -23,13 +53,14 @@ const decks = (state,action) => {
     default:
       return state || [];
   }
-};
+};*/
 
 
 
 const store = Redux.createStore(Redux.combineReducers({
   cards,
-  decks
+  deck,
+  addingDeck
 }));
 
 //retorna false se não houverem cartas
@@ -59,7 +90,18 @@ const Sidebar = React.createClass({
   }
 });
 
+function run(){
+  console.log(state);
+  let state = store.getState();
+  ReactDOM.render(<App>
+    <Sidebar decks={state.decks} addingDeck={state.addingDeck} />
+  </App>, document.getElementById('root'));
+}
 
-ReactDOM.render(<App>
-  <Sidebar decks={[ { name: 'Deck 1' } ]} addingDeck={false} />
-</App>, document.getElementById('root'));
+run();
+
+store.subscribe(run);
+
+window.show = () => store.dispatch(showAddDeck());
+window.hide = () => store.dispatch(hideAddDeck());
+window.add = () => store.dispatch(addDeck( new Date().toString()));
